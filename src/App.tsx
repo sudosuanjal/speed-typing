57406c78f3baf663012ce67ebc2a1bafe13cdbb7
sudosuanjal <anjalehackchey101@gmail.com>
2025-09-07 19:57:59 +0530

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-amber-400">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
